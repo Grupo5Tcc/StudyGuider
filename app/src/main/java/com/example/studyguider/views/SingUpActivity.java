ac0311bba56164e3c5d556ae64fd2ee545aa53e7
EduBarros234 <eduardo.s.barros@unesp.br>
2024-06-07 08:54:57 -0300

    1.3.2 Commit

---
+++ app/src/main/java/com/example/studyguider/views/SingUpActivity.java
@@ -0,0 +1,26 @@
+package com.example.studyguider.views;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.studyguider.R;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sing_up);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
