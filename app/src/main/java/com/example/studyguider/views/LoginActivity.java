cd042ab5b1e2f3f11f657dc39c9eb189789fcec7
EduBarros234 <eduardo.s.barros@unesp.br>
2024-06-11 08:05:21 -0300

    Login and Sing Up Commit

--- app/src/main/java/com/example/studyguider/views/LoginActivity.java
+++ app/src/main/java/com/example/studyguider/views/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.studyguider.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +25,24 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button buttonMenu = findViewById(R.id.btn_menu);
+        Button buttonSingUp = findViewById(R.id.btn_sing_up);
+        buttonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SingUpActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/studyguider/views/MenuActivity.java
+++ app/src/main/java/com/example/studyguider/views/MenuActivity.java
@@ -1,9 +1,6 @@
 package com.example.studyguider.views;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,26 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.studyguider.R;
 
-public class SingUpActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sing_up);
+        setContentView(R.layout.activity_menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button buttonMenu = findViewById(R.id.btn_menu);
-        buttonMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SingUpActivity.this, MenuActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
