40d00eebb900a58d7cf5b3999e997088758eb279
Grupo5tcc <grupotcc408@gmail.com>
2024-06-16 10:46:49 -0300

    Readme Commit

--- app/src/main/java/com/example/studyguider/views/SingUpActivity.java
+++ app/src/main/java/com/example/studyguider/views/SingUpActivity.java
@@ -1,9 +1,12 @@
 package com.example.studyguider.views;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +16,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.studyguider.R;
 
+import java.util.Calendar;
+
 public class SingUpActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,34 @@ public class SingUpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //texts
+        EditText editTextUsername = findViewById(R.id.txt_name);
+        EditText editTextEMail = findViewById(R.id.txt_email);
+        EditText editTextPassword = findViewById(R.id.txt_password);
+        EditText editTextDateOfBirth = findViewById(R.id.txt_date);
+
+        editTextDateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog picker;
+                final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                picker = new DatePickerDialog(SingUpActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        editTextDateOfBirth.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                    }
+                },year,month,day);
+                picker.show();
+            }
+        });
+
+
+        //buttons
 
         Button buttonMenu = findViewById(R.id.btn_menu);
         buttonMenu.setOnClickListener(new View.OnClickListener() {
