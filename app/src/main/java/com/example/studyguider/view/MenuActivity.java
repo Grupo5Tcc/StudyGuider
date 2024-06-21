1fc0f3db0d60481fc47fb25b07a5140c277e0237
ra2257096 <grupotcc405@gmail.com>
2024-06-21 08:54:22 -0300

    1.3.4 Commit

--- app/src/main/java/com/example/studyguider/view/MenuActivity.java
+++ app/src/main/java/com/example/studyguider/view/MenuActivity.java
@@ -1,6 +1,9 @@
 package com.example.studyguider.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +20,8 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
