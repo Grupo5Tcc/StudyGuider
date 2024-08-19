b4216d500cfd2705c3ed6a48861f03d1611921ce
EduBarros234 <eduardo.s.barros@unesp.br>
2024-08-19 10:55:16 -0300

    Emotional Calendar MVVM Commit

--- app/src/main/java/com/example/studyguider/viewmodels/EmotionalCalendarViewModel.java
+++ app/src/main/java/com/example/studyguider/viewmodels/EmotionalCalendarViewModel.java
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
