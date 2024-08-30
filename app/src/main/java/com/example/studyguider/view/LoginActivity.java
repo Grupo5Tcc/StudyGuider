d7eb046c2683c79fd8e73d4f9875d79827eb4db4
maclocal <grupotcc408@gmail.com>
2024-08-30 09:27:31 -0300

    30.08.24 Commit

--- app/src/main/java/com/example/studyguider/view/LoginActivity.java
+++ app/src/main/java/com/example/studyguider/view/LoginActivity.java
@@ -31,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
