a8a418694e19f829e9169303022b86f4fe3a7e49
ra2257083 <jenyffer.butzke@unesp.br>
2024-09-06 08:53:04 -0300

    06.09.2024 Shifts Commit

--- app/src/main/java/com/example/studyguider/view/UserAdapter.java
+++ app/src/main/java/com/example/studyguider/view/UserAdapter.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.studyguider.R;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
