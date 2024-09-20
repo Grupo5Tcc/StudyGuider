0bfa1136d2f005bf9dbe60b082c6184c31a6eaf0
ra2257083 <jenyffer.butzke@unesp.br>
2024-09-20 08:52:01 -0300

    afazeres 20.09 commit

--- app/src/main/java/com/example/studyguider/view/ToDoListActivity.java
+++ app/src/main/java/com/example/studyguider/view/ToDoListActivity.java
@@ -8,8 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageButton; // Certifique-se de importar ImageButton
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.view.LayoutInflater;
@@ -17,46 +15,28 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.studyguider.R;
 import com.example.studyguider.models.TaskItemToDoList;
-import com.example.studyguider.viewmodels.HeaderViewModel;
 import com.example.studyguider.viewmodels.ToDoListViewModel;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ToDoListActivity extends AppCompatActivity {
 
-    private HeaderViewModel headerViewModel;
     private ArrayList<TaskItemToDoList> items;
     private ItemAdapter itemsAdapter;
     private ListView lvItems;
     private EditText editText;
     private ToDoListViewModel viewModel;
-    private boolean selectAllMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
-
-        headerViewModel = new ViewModelProvider(this).get(HeaderViewModel.class);
-
-        View headerView = findViewById(R.id.header);
-        HeaderActivity headerActivity = new HeaderActivity(headerView, headerViewModel, this);
-
-        FirebaseUser currentUser1 = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser1 != null) {
-            headerViewModel.fetchUsername(currentUser1);
-        }
 
         lvItems = findViewById(R.id.item_list);
         editText = findViewById(R.id.edit_text);
@@ -65,7 +45,6 @@ public class ToDoListActivity extends AppCompatActivity {
         lvItems.setAdapter(itemsAdapter);
 
         viewModel = new ViewModelProvider(this).get(ToDoListViewModel.class);
-
         viewModel.getTasks().observe(this, new Observer<List<TaskItemToDoList>>() {
             @Override
             public void onChanged(List<TaskItemToDoList> taskItems) {
@@ -95,66 +74,47 @@ public class ToDoListActivity extends AppCompatActivity {
         Button btnSelectAll = findViewById(R.id.btn_select_all);
         Button btnDeleteSelected = findViewById(R.id.btn_delete_selected);
 
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String newItem = editText.getText().toString();
-                if (!newItem.isEmpty()) {
-                    viewModel.addTask(newItem);
-                    editText.setText("");
-                }
+        btnAdd.setOnClickListener(v -> {
+            String newItem = editText.getText().toString();
+            if (!newItem.isEmpty()) {
+                viewModel.addTask(newItem);
+                editText.setText("");
             }
         });
 
-        btnSelectAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean allChecked = true;
+        btnSelectAll.setOnClickListener(v -> {
+            boolean allChecked = true;
+            for (int i = 0; i < lvItems.getChildCount(); i++) {
+                View item = lvItems.getChildAt(i);
+                CheckBox checkBox = item.findViewById(R.id.checkBox);
+                if (!checkBox.isChecked()) {
+                    allChecked = false;
+                    checkBox.setChecked(true);
+                    viewModel.updateTaskCompletion(items.get(i).getId(), true);
+                }
+            }
+            if (allChecked) {
                 for (int i = 0; i < lvItems.getChildCount(); i++) {
                     View item = lvItems.getChildAt(i);
                     CheckBox checkBox = item.findViewById(R.id.checkBox);
-                    if (!checkBox.isChecked()) {
-                        allChecked = false;
-                        checkBox.setChecked(true);
-                    }
-                }
-                selectAllMode = !allChecked;
-            }
-        });
-
-        btnDeleteSelected.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ArrayList<String> itemsToRemoveIds = new ArrayList<>();
-                for (int i = 0; i < lvItems.getChildCount(); i++) {
-                    View item = lvItems.getChildAt(i);
-                    CheckBox checkBox = item.findViewById(R.id.checkBox);
-                    if (checkBox.isChecked()) {
-                        TaskItemToDoList taskItem = items.get(i);
-                        itemsToRemoveIds.add(taskItem.getId());
-                    }
-                }
-                for (String itemId : itemsToRemoveIds) {
-                    viewModel.deleteTask(itemId);
+                    checkBox.setChecked(false);
+                    viewModel.updateTaskCompletion(items.get(i).getId(), false);
                 }
             }
         });
 
-        ImageView myButton = findViewById(R.id.myButton);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Inicia a MainActivity
-                Intent intent = new Intent(ToDoListActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish(); // Encerra a ProfileActivity
+        btnDeleteSelected.setOnClickListener(v -> {
+            ArrayList<String> itemsToRemoveIds = new ArrayList<>();
+            for (int i = 0; i < lvItems.getChildCount(); i++) {
+                View item = lvItems.getChildAt(i);
+                CheckBox checkBox = item.findViewById(R.id.checkBox);
+                if (checkBox.isChecked()) {
+                    TaskItemToDoList taskItem = items.get(i);
+                    itemsToRemoveIds.add(taskItem.getId());
+                }
+            }
+            for (String itemId : itemsToRemoveIds) {
+                viewModel.deleteTask(itemId);
             }
         });
     }
@@ -175,25 +135,18 @@ public class ToDoListActivity extends AppCompatActivity {
             }
 
             TaskItemToDoList item = items.get(position);
-
             TextView itemText = convertView.findViewById(R.id.item_text);
             CheckBox checkBox = convertView.findViewById(R.id.checkBox);
 
-            // Configure o texto do item
             itemText.setText(item.getTask());
 
-            // Desativa o listener antes de alterar o estado do checkbox para evitar loops infinitos
             checkBox.setOnCheckedChangeListener(null);
-
-            // Define o estado do checkbox baseado no estado do item
             checkBox.setChecked(item.isCompleted());
-
-            // Reativa o listener após o estado do checkbox ser configurado
             checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                // Atualiza o estado de conclusão da tarefa
                 viewModel.updateTaskCompletion(item.getId(), isChecked);
             });
 
+            convertView.setOnClickListener(v -> checkBox.toggle()); // Permite clicar em todo o item
             return convertView;
         }
     }
