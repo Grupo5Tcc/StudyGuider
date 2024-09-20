c8ace7b40f8e9ed1d7d4ff9cbf6cf5622151e4fb
VitoriaAyumi <vitoriayy12@gmail.com>
2024-09-20 09:23:11 -0300

    Vih 20.09 commit

--- app/src/main/java/com/example/studyguider/view/App.java
+++ app/src/main/java/com/example/studyguider/view/App.java
@@ -2,8 +2,10 @@ package com.example.studyguider.view;
 
 import android.app.Application;
 
+import com.example.studyguider.models.Notas;
 import com.example.studyguider.models.Shift;
 
 public class App extends Application {
     public static Shift plantoes;
+    public static Notas notas;
 }
